b43bb79699568e0c6860a1131b6204adad861297
EvgenyBaz <evgeny.bazarov@gmail.com>
2023-07-29 21:07:23 +0300

    homework08 done!

---
+++ HW08/task02/package/__init__.py
@@ -0,0 +1,8 @@
+from .main01 import file_transform_to_json
+from .main02 import json_list
+from .main04 import func
+from .main05 import rename_file
+from .main06 import csv_converter
+
+
+__all__=['file_transform_to_json', 'json_list', 'func', 'rename_file', 'csv_converter']
