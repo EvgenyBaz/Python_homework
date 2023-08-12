0808100a16507f3151954a9c750c5a597221b855
EvgenyBaz <evgeny.bazarov@gmail.com>
2023-08-12 09:06:07 +0300

    homework12 done!!!

--- HW12/task01/main.py
+++ HW12/task01/main.py
@@ -38,7 +38,7 @@ class Student:
 
     def __str__(self):
         return f' студент {self.get_full_name} \n {list(self._course_exams.keys())}'
-
+    @property
     def get_full_name(self):
         return f'{self._first_name} {self._second_name}'
 
@@ -78,37 +78,60 @@ class Student:
 
     def add_test_result(self, course_name, value):
         if course_name not in self._course_tests.keys():
-            raise ValueError(f'у студента {self.get_full_name()} нет такого курса')
+            raise ValueError(f'у студента {self.get_full_name} нет такого курса')
         if value < 1 or value > 100:
             raise ValueError(f'оценка за тест должна быть от 1 до 100')
         self._course_tests[course_name].append(value)
 
     def get_test_result(self, course_name):
         if course_name not in self._course_tests.keys():
-            raise ValueError(f'у студента {self.get_full_name()} нет такого курса')
+            raise ValueError(f'у студента {self.get_full_name} нет такого курса')
         if len(self._course_tests[course_name])!= 0:
             aver_res = sum(self._course_tests[course_name][i] for i in range(len(self._course_tests[course_name])))/len(self._course_tests[course_name])
         else:
             aver_res = -999
         return aver_res
 
+    def get_total_test_result(self):
+        aver_res = 0
+        count = 0
+        for item in self._course_tests.keys():
+            if self.get_test_result(item) != -999:
+                aver_res += self.get_test_result(item)
+                count +=1
+        aver_res = aver_res / count
+        return aver_res
+
+
+def print_res(stu, type_ , course):
+    if type_ == "exam":
+        print(f'Студент {stu.get_full_name} имеет среднюю оценку за экзамены по предмету {course} = {stu.get_exam_result(course)}')
+    if type_ == "test":
+        print(f'Студент {stu.get_full_name} имеет среднюю оценку за тесты по предмету {course} = {stu.get_test_result(course)}')
 
 
 stu1 = Student('Иван', 'Иванов')
 print(stu1)
 
-stu1.add_exam_result("Математика", 5)
-stu1.add_exam_result("Математика", 4)
-print(f'Студент {stu1.get_full_name()} имеет среднюю оценку за экзамены по предмету "Математика" = {stu1.get_exam_result("Математика")}')
+course = "Математика"
+stu1.add_exam_result(course, 5)
+stu1.add_exam_result(course, 4)
+print_res(stu1, "exam", course)
 
-stu1.add_exam_result("История", 3)
-stu1.add_exam_result("История", 4)
-print(f'Студент {stu1.get_full_name()} имеет среднюю оценку за экзамены по предмету "История" = {stu1.get_exam_result("История")}')
+stu1.add_test_result(course, 50)
+stu1.add_test_result(course, 60)
+print_res(stu1, "test", course)
+
+course = "История"
+stu1.add_exam_result(course, 3)
+stu1.add_exam_result(course, 4)
+print_res(stu1, "exam", course)
+
+course = "Литература"
+stu1.add_test_result(course , 99)
+stu1.add_test_result(course , 77)
+print_res(stu1, "test", course)
 
 
-stu1.add_test_result("Физика", 50)
-stu1.add_test_result("Физика", 60)
-print(f'Студент {stu1.get_full_name()} имеет среднюю оценку за тесты по предмету "Физика" = {stu1.get_test_result("Физика")}')
-
-print(f'Студент {stu1.get_full_name()} имеет среднюю оценку за все предметы = {stu1.get_total_exam_result()}')
-
+print(f'Студент {stu1.get_full_name} имеет среднюю оценку  по экзаменам за все предметы = {stu1.get_total_exam_result()}')
+print(f'Студент {stu1.get_full_name} имеет среднюю оценку по тестам за все предметы = {stu1.get_total_test_result()}')
